4b605cde6a7ef5ec7d07ba62f4db87814a7b08d0
Lílian Vieira <lilianv528@gmail.com>
2024-02-29 15:34:28 -0300

    realizando leitura de um arquivo

--- Program.cs
+++ Program.cs
@@ -1,20 +1,20 @@
 ﻿using ExplorandoLinguagem.Models;
 using System.Globalization;
 
-Pessoa p1 = new("Lílian", "Vieira");
-Pessoa p2 = new("vfbf", "queiroz");
+// Pessoa p1 = new("Lílian", "Vieira");
+// Pessoa p2 = new("vfbf", "queiroz");
 
-Curso ingles = new()
-{
-  Nome = "Inglês",
-  Alunos = new List<Pessoa>(),
-};
+// Curso ingles = new()
+// {
+//   Nome = "Inglês",
+//   Alunos = new List<Pessoa>(),
+// };
 
-ingles.AdicionarAluno(p1);
-ingles.AdicionarAluno(p2);
-ingles.ListarAlunos();
+// ingles.AdicionarAluno(p1);
+// ingles.AdicionarAluno(p2);
+// ingles.ListarAlunos();
 
-// -----------------------------------------------------------
+// // -----------------------------------------------------------
 
 // CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
 
@@ -35,18 +35,27 @@ ingles.ListarAlunos();
 
 // Console.WriteLine(data);
 
-string data2 = "2024/02/29 14:00";
+// string data2 = "2024/02/29 14:00";
 
-bool sucesso = DateTime.TryParseExact(data2,
-                        "yyyy/MM/dd HH:mm",
-                        CultureInfo.InvariantCulture,
-                        DateTimeStyles.None, out DateTime dateT);
+// bool sucesso = DateTime.TryParseExact(data2,
+//                         "yyyy/MM/dd HH:mm",
+//                         CultureInfo.InvariantCulture,
+//                         DateTimeStyles.None, out DateTime dateT);
 
-if (sucesso)
+// if (sucesso)
+// {
+//   Console.WriteLine($"Conversão com sucesso! Data: {data2}");
+// }
+// else
+// {
+//   Console.WriteLine($"{data2} não é uma data válida");
+// }
+
+
+string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
+
+foreach (string linha in linhas)
 {
-  Console.WriteLine($"Conversão com sucesso! Data: {data2}");
+  Console.WriteLine(linha);
 }
-else
-{
-  Console.WriteLine($"{data2} não é uma data válida");
-}
+
