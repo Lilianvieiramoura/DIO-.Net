d15aceb9da233e7b9f5a14b3561fcc60e64807ed
Lílian Vieira <lilianv528@gmail.com>
2024-02-29 14:30:24 -0300

    validando retorno do tryparse

--- Program.cs
+++ Program.cs
@@ -16,30 +16,37 @@ ingles.ListarAlunos();
 
 // -----------------------------------------------------------
 
-CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+// CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
 
-decimal valorMonetario = 1582.40M;
-Console.WriteLine(valorMonetario.ToString("C1"));
+// decimal valorMonetario = 1582.40M;
+// Console.WriteLine(valorMonetario.ToString("C1"));
 
-double porcentagem = .3421;
+// double porcentagem = .3421;
 
-Console.WriteLine(porcentagem.ToString("P"));
+// Console.WriteLine(porcentagem.ToString("P"));
 
-DateTime data = DateTime.Parse("29/02/2024 11:00");
+// DateTime data = DateTime.Parse("29/02/2024 11:00");
 
-Console.WriteLine(data.ToString("dd/MM/yyyy HH:mm"));
-Console.WriteLine(data.ToString("yyyy/MM/dd HH:mm"));
+// Console.WriteLine(data.ToString("dd/MM/yyyy HH:mm"));
+// Console.WriteLine(data.ToString("yyyy/MM/dd HH:mm"));
 
-Console.WriteLine(data.ToShortDateString());
-Console.WriteLine(data.ToShortTimeString());
+// Console.WriteLine(data.ToShortDateString());
+// Console.WriteLine(data.ToShortTimeString());
 
-Console.WriteLine(data);
+// Console.WriteLine(data);
 
-string data2 = "2024/02/29 11:00";
+string data2 = "2024/02/29 14:00";
 
-DateTime.TryParseExact(data2,
-                        "yyyy-MM-dd HH:mm",
+bool sucesso = DateTime.TryParseExact(data2,
+                        "yyyy/MM/dd HH:mm",
                         CultureInfo.InvariantCulture,
                         DateTimeStyles.None, out DateTime dateT);
 
-Console.WriteLine(dateT);
+if (sucesso)
+{
+  Console.WriteLine($"Conversão com sucesso! Data: {data2}");
+}
+else
+{
+  Console.WriteLine($"{data2} não é uma data válida");
+}
