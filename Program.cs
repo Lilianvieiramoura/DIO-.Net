89a447c6ebc32c26e90c09c40e2dd9f3393f9e8c
Lílian Vieira <lilianv528@gmail.com>
2024-03-18 10:18:52 -0300

    Introdução a Filas

--- Program.cs
+++ Program.cs
@@ -81,5 +81,24 @@ using System.Globalization;
 // }
 
 
-new ExemploExcecao().Metodo1();
+// new ExemploExcecao().Metodo1();
 
+Queue<int> fila = new();
+
+fila.Enqueue(2);
+fila.Enqueue(4);
+fila.Enqueue(6);
+fila.Enqueue(8);
+
+foreach (int item in fila)
+{
+  Console.WriteLine(item);
+}
+
+Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+fila.Enqueue(10);
+
+foreach (int item in fila)
+{
+  Console.WriteLine(item);
+}
