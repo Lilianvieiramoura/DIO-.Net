e0e4db5c735436dd126ff6b8a87af783f596d51f
Lílian Vieira <lilianv528@gmail.com>
2024-03-18 10:37:35 -0300

    Introdução a pilhas

--- Program.cs
+++ Program.cs
@@ -83,22 +83,43 @@ using System.Globalization;
 
 // new ExemploExcecao().Metodo1();
 
-Queue<int> fila = new();
+// Queue<int> fila = new();
 
-fila.Enqueue(2);
-fila.Enqueue(4);
-fila.Enqueue(6);
-fila.Enqueue(8);
+// fila.Enqueue(2);
+// fila.Enqueue(4);
+// fila.Enqueue(6);
+// fila.Enqueue(8);
 
-foreach (int item in fila)
+// foreach (int item in fila)
+// {
+//   Console.WriteLine(item);
+// }
+
+// Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
+// fila.Enqueue(10);
+
+// foreach (int item in fila)
+// {
+//   Console.WriteLine(item);
+// }
+
+
+Stack<int> pilha = new();
+pilha.Push(4);
+pilha.Push(6);
+pilha.Push(8);
+pilha.Push(10);
+
+foreach (int item in pilha)
 {
   Console.WriteLine(item);
 }
 
-Console.WriteLine($"Removendo o elemento: {fila.Dequeue()}");
-fila.Enqueue(10);
+Console.WriteLine($"Removendo o elemento do topo: {pilha.Pop()}");
 
-foreach (int item in fila)
+pilha.Push(20);
+
+foreach (int item in pilha)
 {
   Console.WriteLine(item);
 }
